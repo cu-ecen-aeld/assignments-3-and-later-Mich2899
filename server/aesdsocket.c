db7f18c080899fca79b5a2a61c0ce4e405496905
Mich2899 <mich1576@colorado.edu>
2021-10-03 00:15:45 -0600

    Part 1

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -9,37 +9,64 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <signal.h>
+#include <linux/fs.h>
+#include <stdbool.h>
+#include <sys/stat.h>
 
 #define MAXSIZE 100
 #define MYPORT "9000"  // the port users will be connecting to
 #define BACKLOG 10     // how many pending connections queue will hold
 
-int storefd, acceptfd;
+int storefd, acceptfd, socketfd;
 
 void sig_handler(int signo)
 {
   if ((signo == SIGINT) || (signo == SIGTERM)){
     printf("Caught signal, exiting\n");
     remove("/var/tmp/aesdsocketdata");
-    close(acceptfd);
+
     close(storefd);
+    close(socketfd);
+    closelog();
     exit(EXIT_SUCCESS);
   }
 }
 
-int main(void){
+int main(int argc, char *argv[]){
 
-	int status, socketfd, bindret, listenret, recvret, writeret, readret, sendret;
+	int status, bindret, listenret, recvret, writeret, readret, sendret;
+	int sendsize=0;
 	struct addrinfo hints;
 	struct addrinfo *res;
 	struct sockaddr_in connection_addr;
 	socklen_t addr_size;
 	char buf[MAXSIZE];
-	char* buf3; char* newline;
+	char* buf3; char* newline; 
 	off_t ret;
 	size_t buf2_size=0;
+	int opt=1;
+        bool daemon = false;	
+	pid_t pid; 
 
-	char *buf2 = (char*)malloc(sizeof(char));	
+	if(argc == 1){
+		daemon = false;
+	}
+
+	else if(argc >2){
+		syslog(LOG_ERR,"Please provide appropriate arguments!!");	
+		return -1;
+	}
+
+	else if(argc == 2){
+		if(strcmp(argv[1],"-d")==0)
+			daemon = true;
+	}
+
+        if (signal(SIGINT, sig_handler) == SIG_ERR)
+              syslog(LOG_ERR,"\ncan't catch SIGINT\n");
+        else if (signal(SIGTERM, sig_handler) == SIG_ERR)
+              syslog(LOG_ERR,"\ncan't catch SIGINT\n");
+
 
 	openlog(NULL,0,LOG_USER);
 	memset(&hints, 0, sizeof(hints)); 		//make sure the struct is empty
@@ -60,7 +87,15 @@ int main(void){
 	    syslog(LOG_ERR,"error: socket!! errno: %s", strerror(errno));
 	    return -1;
 	}
-	
+
+        // Forcefully attaching socket to the port 
+   	if(setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                                                  &opt, sizeof(opt)))
+    	{
+        	perror("setsockopt");
+        	exit(EXIT_FAILURE);
+    	}
+
 	//bind the address and check return value
         bindret = bind(socketfd, res->ai_addr , res->ai_addrlen);
 	if(bindret == -1){
@@ -71,6 +106,36 @@ int main(void){
 	//free the malloc
 	freeaddrinfo(res);
 	
+	
+	        // Forcefully attaching socket to the port
+        if(setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                                                  &opt, sizeof(opt)))
+        {
+                perror("setsockopt");
+                exit(EXIT_FAILURE);
+        }
+
+        if(daemon){
+                syslog(LOG_INFO, "Running in daemon mode!!");
+
+                pid = fork();
+                if (pid == -1)
+                        return -1;
+                else if (pid != 0)
+                        exit (EXIT_SUCCESS);
+                /* create new session and process group */
+                if (setsid () == -1)
+                        return -1;
+                /* set the working directory to the root directory */
+                if (chdir ("/") == -1)
+                        return -1;
+
+                /* redirect fd's 0,1,2 to /dev/null */
+                open ("/dev/null", O_RDWR); /* stdin */
+                dup (0); /* stdout */
+                dup (0); /* stderror */
+        }
+
 	//listen for a connection
 	listenret = listen(socketfd, BACKLOG);
 	if(listenret == -1){
@@ -78,7 +143,16 @@ int main(void){
 	    return -1;
 	}
 
-        addr_size = sizeof connection_addr;
+        //open file
+        storefd = open("/var/tmp/aesdsocketdata", O_CREAT | O_APPEND | O_RDWR, 0666);
+        if(storefd == -1){
+               syslog(LOG_ERR, "error: file open/creation error!! errno:%s", strerror(errno));
+               return -1;
+         }
+
+
+
+       addr_size = sizeof connection_addr;
 
 	while(1){
 		
@@ -90,29 +164,30 @@ int main(void){
                 }
 
                 syslog(LOG_INFO,"Accepted connection from %d", acceptfd);
-
-  		
-		//open file
-                storefd = open("/var/tmp/aesdsocketdata", O_CREAT | O_APPEND | O_RDWR, 0666);
-                if(storefd == -1){
-                    syslog(LOG_ERR, "error: file open/creation error!! errno:%s", strerror(errno));
-                    return -1;
-                }	
 		
-	        memset(buf, 0, MAXSIZE);
+	        memset(buf, '\0', sizeof(buf));
+		char* buf2 = (char*)malloc(MAXSIZE*sizeof(char));
+		int mallocsize = MAXSIZE;
+		char* newptr = NULL;
+
 		do{
-				recvret = recv(acceptfd, buf, MAXSIZE-1, 0);
+				recvret = recv(acceptfd, buf, sizeof(buf), 0);
 				if(recvret == -1){
 					syslog(LOG_ERR, "errno: %s", strerror(errno));
 				}
 				else{
-					buf2_size+=recvret;
-					if(sizeof(buf2)<recvret){
-						buf2 = (char*)malloc(recvret);
+					if((mallocsize-buf2_size) < recvret){
+						mallocsize += recvret;
+						newptr = (char*)realloc(buf2, mallocsize* sizeof(char));
+						if(newptr!=NULL){
+							buf2 = newptr;
+						}
 					}
-					strncpy(buf2, buf, recvret);
+					memcpy(&buf2[buf2_size], buf, recvret);
 				}
-				newline = strchr(buf, '\n');
+
+			buf2_size+=recvret;
+			newline = strchr(buf, '\n');
 		}while(newline == NULL);
 
 		//write into the file
@@ -128,26 +203,32 @@ int main(void){
 				syslog(LOG_ERR, "lseek error!!");
 			}
 
-			
-		buf3 = (char*)malloc(buf2_size);
+		
+		sendsize+=buf2_size;	
+		buf3 = (char*)malloc(sendsize*sizeof(char));
 
-		readret = read(storefd, buf3, buf2_size);
+		readret = read(storefd, buf3, sendsize);
 			if(readret == -1){
 				syslog(LOG_ERR,"read error!!");
 			}	
 	
 	
-		sendret = send(acceptfd, buf3, readret, 0);
+		sendret = send(acceptfd, buf3, sendsize, 0);
 			if(sendret == -1){
 				syslog(LOG_ERR,"send error!!");
 			}
-	   	
+	   free(buf2);
+   	   free(buf3);	   
 
-	      if (signal(SIGINT, sig_handler) == SIG_ERR)
-		  printf("\ncan't catch SIGINT\n");
-	      else if (signal(SIGTERM, sig_handler) == SIG_ERR)
-		  printf("\ncan't catch SIGINT\n");
+	   close(acceptfd);
+	   syslog(LOG_INFO, "Closed connection from %d", acceptfd);
+
+	      buf2_size =0;
+	      recvret=0;
+	      readret=0;
 	}
+
+	close(storefd);
 
 	closelog();
 
